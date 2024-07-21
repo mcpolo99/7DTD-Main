fac17e98d92fff4cd2e42c6ff51eaa86f5f01ed7
mcpolo99 <32239939+mcpolo99@users.noreply.github.com>
2024-07-21 23:21:34 +0200

    1.1.3.79

--- 7d2dMonoInternal/properties/AssemblyInfo.cs
+++ 7d2dMonoInternal/properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.1.3.78")]
+[assembly: AssemblyVersion("1.1.3.79")]
 
 #if DEBUG
 [assembly: AssemblyTitle("Debug")]
@@ -33,6 +33,7 @@ using System.Windows;
 [assembly: AssemblyTitle("UE")]
 [assembly: AssemblyConfiguration("UE")]
 #endif
+
 
 
 
