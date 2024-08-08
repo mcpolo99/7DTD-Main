2d4366c51b62d9a4672643f9459dda317f7b651d
mcpolo99 <markkuw33@gmail.com>
2024-08-08 19:55:39 +0200

    Directory.Build.props changes

--- 7d2dMonoInternal/properties/AssemblyInfo.cs
+++ 7d2dMonoInternal/properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.1.3.108")]
+[assembly: AssemblyVersion("1.1.3.125")]
 
 #if DEBUG
 [assembly: AssemblyTitle("Debug")]
@@ -33,6 +33,23 @@ using System.Windows;
 [assembly: AssemblyTitle("UE")]
 [assembly: AssemblyConfiguration("UE")]
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
